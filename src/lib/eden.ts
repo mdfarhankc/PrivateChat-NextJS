dc91f8d631ef02aff4d36ba18046e745f842708d
mdfarhankc <115876709+mdfarhankc@users.noreply.github.com>
2025-12-10 23:48:05 +0530

    update

--- src/lib/eden.ts
+++ src/lib/eden.ts
@@ -2,6 +2,5 @@ import { treaty } from '@elysiajs/eden';
 import type { app } from '@/app/api/[[...slugs]]/route';
 
 // this require .api to enter /api prefix
-const baseUrl =
-    process.env.NEXT_PUBLIC_API_BASE_URL ?? "http://localhost:3000";
+const baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL as string;
 export const api = treaty<app>(baseUrl).api;
